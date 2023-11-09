3684b9470e7c7f7ffdeb09d7ee8e8eda8d5d0d5b
Élveson Costa <elveson.esc@gmail.com>
2023-11-09 18:46:04 -0300

    rearrange file sticker_pack_repository

--- lib/src/repositories/sticker_pack/sticker_pack_repository_impl.dart
+++ lib/src/repositories/sticker_pack/sticker_pack_repository_impl.dart
@@ -4,7 +4,7 @@ import 'dart:developer';
 import 'package:app_animesticker/src/core/exceptions/repository_exception.dart';
 import 'package:app_animesticker/src/core/fp/either.dart';
 import 'package:app_animesticker/src/model/sticker_pack_model.dart';
-import 'package:app_animesticker/src/repositories/sticker_pack_repository.dart';
+import 'package:app_animesticker/src/repositories/sticker_pack/sticker_pack_repository.dart';
 
 import 'package:flutter/services.dart';
 
