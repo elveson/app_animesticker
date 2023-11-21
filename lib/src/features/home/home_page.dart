05bea283defb721c34885aa56e6182b61bc9d51a
Élveson Costa <elveson.esc@gmail.com>
2023-11-21 20:53:18 -0300

    add menu side

--- lib/src/features/home/home_page.dart
+++ lib/src/features/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:app_animesticker/src/core/ui/constants.dart';
 import 'package:app_animesticker/src/core/widgets/logo.dart';
-import 'package:app_animesticker/src/core/widgets/side_menu.dart';
+import 'package:app_animesticker/src/features/home/widget/side_menu.dart';
 import 'package:app_animesticker/src/features/category/category_page.dart';
 import 'package:app_animesticker/src/features/feed/feed_page.dart';
 
@@ -45,16 +45,142 @@ class _HomePageState extends State<HomePage> {
             onPressed: () {},
           ),
         ],
-        leading: IconButton(
-          icon: const ImageIcon(
-            AssetImage('assets/icons/menu_left.png'),
-            color: ColorsConstantsLight.colorText,
-          ),
-          tooltip: 'Menu de navegação',
-          onPressed: () {},
-        ),
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const ImageIcon(
+              AssetImage('assets/icons/menu_left.png'),
+              color: ColorsConstantsLight.colorText,
+            ),
+            tooltip: 'Menu de navegação',
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        }),
       ),
-      // drawer: const SideMenu(),
+      drawer: const SideMenu(),
+      // Drawer(
+      //   elevation: 0,
+      //   backgroundColor: ColorsConstantsLight.colorbackgoundMenu,
+      //   child: ListView(
+      //     children: [
+      //       const SizedBox(
+      //          height: 56,
+      //       ),
+      //       Container(
+      //         decoration: const BoxDecoration(
+      //           gradient: LinearGradient(
+      //             begin: Alignment.centerRight,
+      //             end: Alignment.centerLeft,
+      //             stops: [0.075, 0.9512],
+      //             colors: [
+      //               Color(0xFFBA0000),
+      //               Color(0xFFFF3636),
+      //             ],
+      //           ),
+      //         ),
+      //         child: ListTile(
+      //           leading: const ImageIcon(
+      //             AssetImage(
+      //               'assets/icons/chess.png',
+      //             ),
+      //             color: ColorsConstantsLight.colorSurface,
+      //           ),
+      //           title: const Text(
+      //             "Be Ottafan",
+      //             style: TextStyle(
+      //               fontFamily: FontConstants.fontFamily,
+      //               fontSize: 17,
+      //               letterSpacing: -0.408,
+      //               color: ColorsConstantsLight.colorSurface,
+      //             ),
+      //           ),
+      //           onTap: () {},
+      //         ),
+      //       ),
+      //       const Padding(
+      //         padding: EdgeInsets.symmetric(horizontal: 24, vertical: 17.5),
+      //         child: Text(
+      //           'Follow Us',
+      //           style: TextStyle(
+      //             fontFamily: FontConstants.fontFamily,
+      //             fontSize: 16,
+      //             letterSpacing: -0.32,
+      //             color: ColorsConstantsLight.subtle1,
+      //           ),
+      //         ),
+      //       ),
+      //       ListTile(
+      //         leading: const ImageIcon(
+      //           AssetImage('assets/icons/instagram.png'),
+      //         ),
+      //         title: const Text("Instagram"),
+      //         onTap: () {},
+      //       ),
+      //       ListTile(
+      //         leading: const ImageIcon(
+      //           AssetImage('assets/icons/tiktok.png'),
+      //         ),
+      //         title: const Text("TikTok"),
+      //         onTap: () {},
+      //       ),
+      //       ListTile(
+      //         leading: const ImageIcon(
+      //           AssetImage('assets/icons/world.png'),
+      //         ),
+      //         title: const Text("Ottamaker project"),
+      //         onTap: () {},
+      //       ),
+      //       const Padding(
+      //         padding: EdgeInsets.symmetric(horizontal: 24, vertical: 17.5),
+      //         child: Text(
+      //           'App Info',
+      //           style: TextStyle(
+      //             fontFamily: FontConstants.fontFamily,
+      //             fontSize: 16,
+      //             letterSpacing: -0.32,
+      //             color: ColorsConstantsLight.subtle1,
+      //           ),
+      //         ),
+      //       ),
+      //       ListTile(
+      //         leading: const ImageIcon(
+      //           AssetImage('assets/icons/more.png'),
+      //         ),
+      //         title: const Text("Collaborate"),
+      //         onTap: () {},
+      //       ),
+      //       ListTile(
+      //         leading: const ImageIcon(
+      //           AssetImage('assets/icons/category.png'),
+      //         ),
+      //         title: const Text("More apps"),
+      //         onTap: () {},
+      //       ),
+      //       ListTile(
+      //         leading: const ImageIcon(
+      //           AssetImage('assets/icons/bug.png'),
+      //         ),
+      //         title: const Text("Report a bug"),
+      //         onTap: () {},
+      //       ),
+      //       ListTile(
+      //         leading: const ImageIcon(
+      //           AssetImage('assets/icons/paper_privacy.png'),
+      //         ),
+      //         title: const Text("Terms of privacy"),
+      //         onTap: () {},
+      //       ),
+      //       ListTile(
+      //         leading: const ImageIcon(
+      //           AssetImage('assets/icons/paper_contract.png'),
+      //         ),
+      //         title: const Text("Terms of use"),
+      //         onTap: () {},
+      //       ),
+      //     ],
+      //   ),
+      // ),
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: const [
