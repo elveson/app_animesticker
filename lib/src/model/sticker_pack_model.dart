81b5196fa815431980052905e7ded0bfa2e51b8e
Élveson Costa <elveson.esc@gmail.com>
2023-11-30 17:35:34 -0300

    Add animeIdentifier field to StickerPackModel

--- lib/src/model/sticker_pack_model.dart
+++ lib/src/model/sticker_pack_model.dart
@@ -2,6 +2,7 @@ import 'package:app_animesticker/src/model/sticker_model.dart';
 
 class StickerPackModel {
   final String identifier;
+  final String? animeIdentifier;
   final String name;
   final String publisher;
   final String trayImageFile;
@@ -14,6 +15,7 @@ class StickerPackModel {
 
   StickerPackModel({
     required this.identifier,
+    this.animeIdentifier,
     required this.name,
     required this.publisher,
     required this.trayImageFile,
@@ -21,7 +23,7 @@ class StickerPackModel {
     required this.publisherWebsite,
     required this.privacyPolicyWebsite,
     required this.licenseAgreementWebsite,
-    this.animatedStickerPack = false,
+    this.animatedStickerPack,
     required this.stickers,
   });
 
@@ -40,6 +42,7 @@ class StickerPackModel {
       } =>
         StickerPackModel(
           identifier: identifier,
+          animeIdentifier: json['anime_identifier'] ?? '',
           name: name,
           publisher: publisher,
           trayImageFile: trayImageFile,
@@ -47,6 +50,7 @@ class StickerPackModel {
           publisherWebsite: publisherWebsite,
           privacyPolicyWebsite: privacyPolicyWebsite,
           licenseAgreementWebsite: licenseAgreementWebsite,
+          animatedStickerPack: json['animated_sticker_pack'] ?? false,
           stickers: List<StickerModel>.from(
             stickers.map((e) => StickerModel.fromMap(e)).toList(),
           ),
